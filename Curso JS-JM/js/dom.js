217f7ef4f1a2c2c4308e22790467fc059af48067
elianmtg <elianmtoribio@gmail.com>
2022-01-07 00:48:12 -0400

    BOM: Propiedades y Eventos

--- Curso JS-JM/js/dom.js
+++ Curso JS-JM/js/dom.js
@@ -546,3 +546,52 @@ document.addEventListener("click", (e)=>{
 //esta es la manera mas optima de trabajar con los eventos 
 //en JavaScript
 
+/* CAP 77. BOM: Propiedades y Eventos */
+
+/*
+El evento DOMContentLoaded es disparado cuando el documento
+HTML ha sido completamente cargado y parseado, sin esperar 
+hojas de estilo, imágenes y subtramas para finalizar la 
+carga.
+El evento load se dispara cuando se ha detectado la carga 
+completa de la página.
+Es un error frecuente usar load cuando DOMContentLoaded es 
+mucho más apropiado.
+Peticiones asíncronas pausan el parseo del DOM.
+*/
+
+window.addEventListener("resize", e => {
+  console.clear();
+  console.log("EVENTO RESIZE");
+  console.log(window.innerWidth);
+  console.log(window.innerHeight);
+  console.log(window.outerWidth);
+  console.log(window.outerHeight);
+  console.log(window.scrollX);
+  console.log(window.scrollY);
+  console.log(e);
+});
+
+window.addEventListener("scroll", e => {
+  console.clear();
+  console.log("EVENTO SCROLL");
+  console.log(window.scrollX);
+  console.log(window.scrollY);
+  console.log(e);
+});
+
+window.addEventListener("load", (e) => {
+  console.log("Evento Load");
+  console.log(window.screenX);
+  console.log(window.screenY);
+  console.log(e);
+});
+
+//mas eficiente
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("Evento DOMContentLoaded");
+  console.log(window.screenX);
+  console.log(window.screenY);
+  console.log(e);
+});
+
