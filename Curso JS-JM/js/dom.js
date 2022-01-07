9f155f3e5babec97d7dfd3a51a282ca5dbb9b502
elianmtg <elianmtoribio@gmail.com>
2022-01-07 01:15:45 -0400

    BOM: Objetos: URL, Historial y Navegador

--- Curso JS-JM/js/dom.js
+++ Curso JS-JM/js/dom.js
@@ -621,3 +621,38 @@ $btnImprimir.addEventListener("click", e=>{
   window.print();
 });
 
+/* CAP 79. BOM: Objetos: URL, Historial y Navegador */
+
+console.log("Objeto URL (location)");
+console.log(location);
+console.log(location.origin);
+console.log(location.protocol);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.port);
+console.log(location.href);
+console.log(location.hash);
+console.log(location.search);
+console.log(location.pathname);
+//location.reload();
+
+console.log("Objeto Historial (history)");
+console.log(history);
+console.log(history.length);
+//history.forward(1);
+//history.go(-3);
+//history.back(2);
+
+console.log("Objeto Navegador (navigator)");
+console.log(navigator);
+console.log(navigator.connection);
+console.log(navigator.geolocation);
+console.log(navigator.mediaDevices);
+console.log(navigator.mimeTypes);
+console.log(navigator.onLine);
+console.log(navigator.serviceWorker);
+console.log(navigator.storage);
+console.log(navigator.usb);
+console.log(navigator.userAgent);
+
+
