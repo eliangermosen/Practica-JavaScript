ea3595e2c25a80915b8526383f75ec2d372a9aa6
elianmtg <elianmtoribio@gmail.com>
2022-01-19 17:35:32 -0400

    DOM: Ejercicios Prácticos Presentacion y Responsive Slider

---
+++ Curso JS-JM/js/index_dom.js
@@ -0,0 +1,8 @@
+import slider from "./dom/carrusel.js";
+
+const d = document;
+
+// este evento representa la carga del documento
+d.addEventListener("DOMContentLoaded", (e) => {
+    slider();
+});
