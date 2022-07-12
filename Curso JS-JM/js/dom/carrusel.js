9977925b2e4ff4a6cc4075671a87293086b6952c
elianmtg <elianmtoribio@gmail.com>
2022-07-12 18:24:16 -0400

    API REST: JSON Server - API falsa local

--- Curso JS-JM/js/dom/carrusel.js
+++ Curso JS-JM/js/dom/carrusel.js
@@ -34,4 +34,56 @@ export default function slider(){
 
         }
     })
+    /* d.addEventListener("keyup", (e) => {
+        if(e.keyCode  === 37){
+            e.preventDefault();
+            $slides[i].classList.remove("active");
+            i--;
+
+            if(i<0){
+                i=$slides.length - 1;
+            }
+
+            $slides[i].classList.add("active");
+
+        }
+        if(e.keyCode  === 39){
+            e.preventDefault();
+            $slides[i].classList.remove("active");
+            i++;
+
+            if(i >= $slides.length){
+                i = 0;
+            }
+
+            $slides[i].classList.add("active");
+
+        }
+    }) */
+    d.addEventListener("keyup", (e) => {
+        if(e.key === "ArrowLeft"){
+            e.preventDefault();
+            $slides[i].classList.remove("active");
+            i--;
+
+            if(i<0){
+                i=$slides.length - 1;
+            }
+
+            $slides[i].classList.add("active");
+
+        }
+        if(e.key === "ArrowRight"){
+            e.preventDefault();
+            $slides[i].classList.remove("active");
+            i++;
+
+            if(i >= $slides.length){
+                i = 0;
+            }
+
+            $slides[i].classList.add("active");
+
+        }
+    })
 }
